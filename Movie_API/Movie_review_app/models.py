116972b143903c68c2525a9311446ef83c6f93e5
Manzi Ivan <manzi9ivan@gmail.com>
2026-02-25 06:39:17 +0200

    Removed unnessesary models and added viewsets that will allow access to the models

--- Movie_API/Movie_review_app/models.py
+++ Movie_API/Movie_review_app/models.py
@@ -2,23 +2,13 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
-class Movie(models.Model):
-    movie_title = models.CharField(max_length=200)
-    movie_description = models.TextField(blank=True)
-    movie_release_date = models.DateTimeField(auto_now_add=True)
-    movie_post_URL = models.URLField(blank=True, null=True)
-    
-
-
-class Rating(models.Model):
-    movie_title = models.OneToOneField(Movie, on_delete=models.CASCADE)
-    movie_score = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), 
-            MaxValueValidator(5)])
-
 class Reviews(models.Model):
-    movie_title = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    content_review = models.TextField(blank=True)
+    movie_title = models.CharField(max_length=255)
+    content_review = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     review_created_at = models.DateTimeField(auto_now_add=True)
-    review_score = models.OneToOneField(Rating, on_delete=models.CASCADE)
+    review_score = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), 
+            MaxValueValidator(5)])
 
+    def __str__(self):
+        return f"{self.movie_title} - {self.user.username} - {self.review_score}"
--- Movie_API/Movie_review_app/views.py
+++ Movie_API/Movie_review_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Reviews
+from rest_framework import viewsets
+from .Serializers import ReviewsSerializer
+from rest_framework import generics
 
-# Create your views here.
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+
+
